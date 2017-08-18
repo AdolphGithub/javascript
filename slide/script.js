f8d692156d5f71fc23d771e83ec8d5871ad1437b
adolph <1138027478@qq.com>
2017-08-18 17:10:33 +0800

    布局

---
+++ slide/script.js
@@ -0,0 +1,25 @@
+(function (window,document) {
+    function Slider(element,options){
+        var ele = document.getElementById(element);
+        this._init(element,options);
+    }
+    // 初始化方法.
+    Slider.prototype._init=function(element,options){
+
+    };
+    // 移动.
+    Slider.prototype._move = function(){
+
+    };
+    // 查找.
+    Slider.prototype._find = function(){
+
+    };
+    // clone最后一个.
+    Slider.prototype._clone = function(){
+
+    };
+
+    // 挂载到window对象.
+    window.Slider = Slider;
+})(window,document);
